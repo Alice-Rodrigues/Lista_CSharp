aed2d44b4787fd17dc59ff4736ac91be7eba375d
Alice00101101Rodrigues <54628916+Alice-Rodrigues@users.noreply.github.com>
2025-10-29 16:44:31 -0400

    Create Exercicio02.cs

---
+++ Exercicio02.cs
@@ -0,0 +1,17 @@
+using System;
+class Program 
+{
+   static void Main() 
+   {
+     Console.Write("Digite o primeiro número: ");
+     double n1 = Convert.ToDouble(Console.ReadLine());
+     
+     Console.Write("Digite o segundo número: ");
+     double n2 = Convert.ToDouble(Console.ReadLine());
+     
+     Console.WriteLine($"Soma: {n1 + n2}");
+     Console.WriteLine($"Subtração: {n1 - n2}");
+     Console.WriteLine($"Multiplicação: {n1 * n2}");
+     Console.WriteLine($"Divisão: {n1 / n2}");
+   }
+}
