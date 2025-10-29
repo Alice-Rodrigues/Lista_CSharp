b0c8dd35b84106d0712e8d2da3cd60e48097a155
Alice00101101Rodrigues <54628916+Alice-Rodrigues@users.noreply.github.com>
2025-10-29 16:46:05 -0400

    Add program to check if a number is even or odd

--- Exercicio03.cs
+++ Exercicio03.cs
@@ -1,1 +1,14 @@
+using System;
+class Program 
+{
+   static void Main() 
+   {
+     Console.Write("Digite um número: ");
+     int num = Convert.ToInt32(Console.ReadLine());
+     if (num % 2 == 0)
+     Console.WriteLine("Par");
+     else
+     Console.WriteLine("Ímpar");
+   }
+}
 
